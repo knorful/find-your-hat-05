570ec64a64a1a302e111925149635ea8afae1cd9
Kris Norful <krisnorful@gmail.com>
2021-07-14 15:38:07 -0500

    create Field class and add print method to display state of field

--- main.js
+++ main.js
@@ -1,6 +1,31 @@
-const prompt = require('prompt-sync')({sigint: true});
+const prompt = require("prompt-sync")({ sigint: true });
 
-const hat = '^';
-const hole = 'O';
-const fieldCharacter = '░';
-const pathCharacter = '*';
+const hat = "^";
+const hole = "O";
+const fieldCharacter = "░";
+const pathCharacter = "*";
+
+class Field {
+  constructor(field) {
+    this._field = field;
+  }
+
+  print() {
+    for (let i = 0; i < this._field.length; i++) {
+      let row = [];
+      let pos = this._field[i];
+      for (let j = 0; j < pos.length; j++) {
+        row.push(pos[j]);
+      }
+      console.log(row.join(""));
+    }
+  }
+}
+
+const myField = new Field([
+  ["*", "░", "O"],
+  ["░", "O", "░"],
+  ["░", "^", "░"],
+]);
+
+myField.print();
